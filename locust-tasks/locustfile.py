27b6a172a3a6691f784b0ff4d8ad3c4e58054ccc
Juan Velasco <jvelasco@iti.es>
2023-11-11 16:31:40 +0100

    Add wait time to he locustfile.py

--- locust-tasks/locustfile.py
+++ locust-tasks/locustfile.py
@@ -1,6 +1,8 @@
-from locust import HttpUser, task
+from locust import HttpUser, task, between
 
 class HelloWorldUser(HttpUser):
     @task
     def hello_world(self):
         self.client.get("/es/blog/")
+
+    wait_time = between(0.5, 10)
